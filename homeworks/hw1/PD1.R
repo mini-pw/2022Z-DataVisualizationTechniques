8223039c7ade746b10eebf0bb8e1784afbbdd405
Anna Kozak <anna1993kozak@gmail.com>
2021-10-10 17:52:36 +0200

    add hw1 file

---
+++ homeworks/hw1/PD1.R
@@ -0,0 +1,71 @@
+library(PogromcyDanych)
+library(dplyr)
+
+colnames(auta2012)
+dim(auta2012)
+head(auta2012[,-ncol(auta2012)])
+sum(is.na(auta2012))
+
+## 1. Z którego rocznika jest najwięcej aut i ile ich jest?
+
+
+## Odp: 
+
+
+## 2. Która marka samochodu występuje najczęściej wśród aut wyprodukowanych w 2011 roku?
+
+
+## Odp:
+
+
+## 3. Ile jest aut z silnikiem diesla wyprodukowanych w latach 2005-2011?
+
+
+## Odp:
+
+
+## 4. Spośród aut z silnikiem diesla wyprodukowanych w 2011 roku, która marka jest średnio najdroższa?
+
+
+## Odp:
+
+
+## 5. Spośród aut marki Skoda wyprodukowanych w 2011 roku, który model jest średnio najtańszy?
+
+
+## Odp:
+
+
+## 6. Która skrzynia biegów występuje najczęściej wśród 2/3-drzwiowych aut,
+##    których stosunek ceny w PLN do KM wynosi ponad 600?
+
+
+## Odp: 
+
+
+## 7. Spośród aut marki Skoda, który model ma najmniejszą różnicę średnich cen 
+##    między samochodami z silnikiem benzynowym, a diesel?
+
+
+## Odp: 
+
+
+## 8. Znajdź najrzadziej i najczęściej występujące wyposażenie/a dodatkowe 
+##    samochodów marki Lamborghini
+
+
+## Odp: 
+
+
+## 9. Porównaj średnią i medianę mocy KM między grupami modeli A, S i RS 
+##    samochodów marki Audi
+
+
+## Odp:
+
+
+## 10. Znajdź marki, których auta występują w danych ponad 10000 razy.
+##     Podaj najpopularniejszy kolor najpopularniejszego modelu dla każdej z tych marek.
+
+
+## Odp: 
