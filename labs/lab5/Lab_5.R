2ae539451a0427e1c742fc5a8a0739b6536dd481
Anna Kozak <anna1993kozak@gmail.com>
2021-11-03 11:36:19 +0100

    update lab5

--- labs/lab5/Lab_5.R
+++ labs/lab5/Lab_5.R
@@ -8,42 +8,42 @@ library(dplyr)
 library(SmarterPoland)
 
 ## Zadanie 1
-# Z zamieszczonego pliku pdf w folderze lab5 należy wybrać jedno z dwóch zadań. Dane potrzbne do odtowrzenia wizualizacji 
-# należy wczytać następująco:
+# Z zamieszczonego pliku .pdf w folderze lab5 należy rozwiązać jedno z dwóch zadań. 
+# Dane potrzbne do odtowrzenia wizualizacji wczytujemy następująco:
 
 df <- read.csv(file = "https://raw.githubusercontent.com/R-Ladies-Warsaw/PoweR/master/Cz%C4%99%C5%9B%C4%87%202%20-%20Formatowanie%20danych/R/data/ranking.csv", 
                encoding = "UTF-8")
-
-
-## ggrepel
-
-### Etykiety tekstowe
-
-library(ggrepel)
-
-#Więcej: https://ggrepel.slowkow.com/articles/examples.html
-
-
-
-## Zadanie 2
-# Narysuj wykres punktowy zależności między wskaźnikiem urodzeń a wskaźnikiem śmiertleności 
-# oraz podpisz punkty nazwą kraju o najniższym i najwyższym wskaźniku śmiertelności.
-
 
 ## patchwork
 
 library(patchwork)
 
 
+## ggrepel
 
-## Zadanie 3 - wykres gęstości brzegowych
-# a) wykres punktowy dwóch zmiennych + kolor
-# b) rozkład jednej zmiennej x 2
+library(ggrepel)
+
+
+# Więcej: https://ggrepel.slowkow.com/articles/examples.html
+
+
+## Zadanie 2
+# Narysuj wykres punktowy zależności między wskaźnikiem urodzeń a wskaźnikiem śmierci 
+# oraz podpisz punkty o najniższym i najwyższym wskaźniku śmiertelności (nazwą kraju).
+
+
+
+## Zadanie 3 - stworzyć wykres gęstości brzegowych:
+# a) wykres punktowy dwóch wskaźników + kolor
+# b) dodać po lewej rozkład zmiennej death.rate
+# c) dodać na dole rozkład zmiennej birth.rate
+
 
 
 ## ggstatsplot
 
 library(ggstatsplot)
+
 
 # Więcej: https://indrajeetpatil.github.io/ggstatsplot/index.html
 
@@ -54,9 +54,11 @@ require(maps)
 
 # maps::county(), maps::france(), maps::italy(), maps::nz(), maps::state(), maps::usa(), maps::world(), maps::world2()
 
-?coord_map()
+
 
 # Więcej: https://ggplot2.tidyverse.org/reference/coord_map.html
 
 ## Zadanie 4
 # Wykorzystując dane countries narysuj mapę świata i zaznacz na niej wskaźnik urodzeń. 
+
+
