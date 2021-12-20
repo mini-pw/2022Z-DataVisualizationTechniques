a410a92bc19c3946609ea25b3050d7103a68399f
DawidPludowski <72541839+DawidPludowski@users.noreply.github.com>
2021-12-20 19:16:15 +0100

    Add files via upload

--- homeworks/hw7/PludowskiDawid/HW7.R
+++ homeworks/hw7/PludowskiDawid/HW7.R
@@ -10,13 +10,16 @@ df <- data.frame(x = x, y = y)
 
 
 ggplot(df) +
-  geom_line( aes(x = x, y = y, color = y) ) +
+  geom_line( aes(x = x, y = y, color = y) , size = 1) +
+  geom_point(aes(x, y), color = "#00e600") + 
   geom_star(
-    aes(x = x, y = y),
-    starshape = 2, color = "yellow"
+    aes(x = runif(x, -0.01, 0.01), y = runif(y, 0, 500)),
+    starshape = 2, color = "yellow", fill = "yellow"
     ) + 
   scale_color_gradient(low = "#66ff33", high = "#00cc00") +
-  xlim(c(-0.01, 0.01)) + 
-  theme_dark()
+  xlim(c(-0.005, 0.005)) + 
+  ylim(c(10, 50)) + 
+  theme_dark() + 
+  theme(legend.position='none')
 
 
